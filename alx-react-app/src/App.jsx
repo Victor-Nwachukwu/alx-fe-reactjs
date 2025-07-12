a764076ce3a7222427865d270d4ee61e33d1656f
Victor Nwachukwu <vixkudos@gmail.com>
2025-07-12 21:12:29 +0100

    react props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -37,6 +38,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Victor" age="37" bio="I love to code and build amazing apps" />
 
     </>
   )
