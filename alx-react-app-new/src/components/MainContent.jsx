6a7400612d1afeb2831519fb32a80e1754f5b2a9
Victor Nwachukwu <vixkudos@gmail.com>
2025-07-17 22:39:43 +0100

    modified details for the styling

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent () {
     return (
         <main>
-            <p style={{ backgroundColor: 'violet', color: 'white', fontFamily: 'sans-serif', fontWeight: 'bolder', fontSize: '20px'}}>I love to visit <span style={{ color: 'navy', fontWeight: 'bolder'}}> New York, Paris, and Tokyo.</span></p>
+            <p style={{ backgroundColor: 'violet', color: 'white', fontFamily: 'sans-serif', fontWeight: 'bolder', fontSize: '20px'}}> I love to visit New York, Paris, and Tokyo.</p>
         </main>
     )
 }
