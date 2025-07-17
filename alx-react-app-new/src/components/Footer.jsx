1a8bff2a4623ac721777f1ca4837e58c2ac0f436
Victor Nwachukwu <vixkudos@gmail.com>
2025-07-17 22:26:19 +0100

    inline styling for the different components in react app

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+    return (
+        <footer>
+            <p style={{ fontFamily: 'sans-serif', color: 'navy', fontWeight: 'bold'}}>© 2023 City Lovers</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent () {
+    return (
+        <main>
+            <p style={{ backgroundColor: 'violet', color: 'white', fontFamily: 'sans-serif', fontWeight: 'bolder', fontSize: '20px'}}>I love to visit <span style={{ color: 'navy', fontWeight: 'bolder'}}> New York, Paris, and Tokyo.</span></p>
+        </main>
+    )
+}
+
+export default MainContent
