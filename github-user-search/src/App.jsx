f0b3726ef794d0c2537a73c980e69da42c2e8ba8
Victor Nwachukwu <vixkudos@gmail.com>
2025-08-02 23:57:31 +0100

    GitHub User Search Application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import Search from "./components/Search";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1 className="text-center text-4xl font-bold mt-10 mb-6">
+        GitHub User Search
+      </h1>
+      <Search />
+    </div>
+  );
+}
